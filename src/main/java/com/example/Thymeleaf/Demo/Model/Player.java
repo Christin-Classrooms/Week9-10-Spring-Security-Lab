8f5d4cdabd17cca4380195cfd1080f7f84c5c278
HailuBen <b.hailu.pk@gmail.com>
2026-03-25 17:07:03 -0400

    add password and role

--- src/main/java/com/example/Thymeleaf/Demo/Model/Player.java
+++ src/main/java/com/example/Thymeleaf/Demo/Model/Player.java
@@ -20,13 +20,17 @@ public class Player {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
     @Size(min=2, max=240, message="name size must be > 2 and <240")
+
     @NotBlank(message="The name is required")
     private String name;
+
     @NotBlank(message = "The email is not required")
     @Email(message = "invalid email")
     private String email;
 
+    private String password;
 
+    private String role;
 
 
 }
