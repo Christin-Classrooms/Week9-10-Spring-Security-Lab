395e562ee7a6df9f3d4be3ee018bde5f6bb58fbd
HailuBen <b.hailu.pk@gmail.com>
2026-03-25 17:27:32 -0400

    add login page + controller method

--- src/main/java/com/example/Thymeleaf/Demo/controllers/HomeController.java
+++ src/main/java/com/example/Thymeleaf/Demo/controllers/HomeController.java
@@ -1,8 +1,11 @@
 package com.example.Thymeleaf.Demo.controllers;
 
+import com.example.Thymeleaf.Demo.Model.Player;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class HomeController {
@@ -13,4 +16,24 @@ public class HomeController {
         model.addAttribute("message", "Hello From Cpan 228 Java Code sent the text");
         return "Home";
     }
+
+    @GetMapping("/register")
+    public String showRegisterForm(Model model) {
+        model.addAttribute("player", new Player());
+        return "register";
+    }
+
+    @GetMapping("/login")
+    public String login() {
+        return "login";
+    }
+
+    @PostMapping("/register")
+    public String processRegister(@ModelAttribute Player player) {
+        player.setPassword(passwordEncoder.encode(player.getPassword()));
+        player.setRole("PLAYER");
+        playerRepository.save(player);
+        return "redirect:/login";
+    }
+
 }
