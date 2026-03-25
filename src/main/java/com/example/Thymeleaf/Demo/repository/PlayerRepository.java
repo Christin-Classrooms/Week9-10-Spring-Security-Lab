7d41d183a9f3a007486fbad3d33d181268e7ca0e
HailuBen <b.hailu.pk@gmail.com>
2026-03-25 17:46:23 -0400

    fix optional findByEmail

--- src/main/java/com/example/Thymeleaf/Demo/repository/PlayerRepository.java
+++ src/main/java/com/example/Thymeleaf/Demo/repository/PlayerRepository.java
@@ -4,15 +4,11 @@ import com.example.Thymeleaf.Demo.Model.Player;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
 import java.util.Optional;
 
-@Repository
 public interface PlayerRepository extends JpaRepository<Player, Integer> {
 
     Page<Player> findByNameContainingIgnoreCase(String name, Pageable page);
 
-    // Use Optional so .orElseThrow() works
     Optional<Player> findByEmail(String email);
 }
