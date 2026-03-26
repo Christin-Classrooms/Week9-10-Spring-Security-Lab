e6bb48928c47cf74244068a17f73737dad1df08b
Aviish11 <avish.auckloo1108@gmail.com>
2026-03-25 21:22:39 -0400

    lab 5 and 6 completed

---
+++ src/main/java/com/example/Thymeleaf/Demo/config/DataLoader.java
@@ -0,0 +1,32 @@
+package com.example.Thymeleaf.Demo.config;
+
+import com.example.Thymeleaf.Demo.Model.User;
+import com.example.Thymeleaf.Demo.repository.UserRepository;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Component;
+
+@Component
+public class DataLoader implements CommandLineRunner {
+
+    private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
+
+    public DataLoader(UserRepository userRepository, PasswordEncoder passwordEncoder) {
+        this.userRepository = userRepository;
+        this.passwordEncoder = passwordEncoder;
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+
+        if (userRepository.findByUsername("admin").isEmpty()) {
+            User admin = new User();
+            admin.setUsername("admin");
+            admin.setPassword(passwordEncoder.encode("admin123"));
+            admin.setRole("ADMIN");
+            userRepository.save(admin);
+        }
+    }
+}
